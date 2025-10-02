7db75427e60d1550b9e84a22ab71f770611c832a
Mikhail Zelenchuck <zelmisha2@gmail.com>
2025-10-02 21:45:47 +0300

    Update settings.py

--- neural_future_backend/neural_future_backend/settings.py
+++ neural_future_backend/neural_future_backend/settings.py
@@ -137,7 +137,7 @@ CELERY_RESULT_BACKEND = f'redis://redis:6379/0'
 
 
 DEEPSEEK_API_KEY = os.getenv(
-    'DEEPSEEK_API_KEY', 'sk-7e20906faf2a43f59eca57d7d8d91649'
+    'DEEPSEEK_API_KEY'
 )
 if not DEEPSEEK_API_KEY:
     raise ValueError("DEEPSEEK_API_KEY must be set")
